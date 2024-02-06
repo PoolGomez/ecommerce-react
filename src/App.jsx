851a64d4181a76efd2001dc86e62bdaec59cf24f
unknown <pgomez4790@gmail.com>
2024-02-05 21:51:07 -0500

    Inicia el manejo de variables de entorno:

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { API_URL } from "../env"
 
 function App() {
 
   return (
     
       <div>
-        <h1>hola mundo</h1>
+        <h1>hola mundo {API_URL}</h1>
       </div>
       
   )
